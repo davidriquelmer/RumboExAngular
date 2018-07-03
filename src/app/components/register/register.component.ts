fec26eb961c7b8676eebc47a802058c170d186e8
kristalys <kristalys.cba4@gmail.com>
2018-07-03 16:06:37 -0400

    Merge branch 'master' of https://github.com/kristalys47/RumboExAngular
    Conflicts:
    	src/app/app.module.ts
    
    	SessionStorage saves if the user is available or not.
    
    	Admin login and Registration test work(subiendo los files que faltan

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+import {User} from '../../models/user';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+user: User = new User();
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+
+  registration(): void {
+    this.auth.register(this.user)
+    .then((user) => {
+      console.log(user)
+      this.router.navigate(['/main']);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+}
